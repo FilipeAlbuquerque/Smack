5ff6dd545d0fb241c8a6a6f24a7c5431aa8daa89
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-07-12 14:10:25 -0300

    create user activity

---
+++ app/src/main/java/com/filipe/smack/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.filipe.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar (view: View){
+
+    }
+
+    fun generateBackgroundClicked (view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
+    }
+
+}
